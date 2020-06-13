d0fd0a9cdaa33a4c73e0a4d5f66e4a511fb56e37
Justin Mitchel <j@teamcfe.com>
2020-06-13 12:09:26 -0700

    Added project & auth module shortcut

--- tightai/__init__.py
+++ tightai/__init__.py
@@ -1,3 +1,17 @@
 __version__ = "1.0.9"
 
+import importlib
+
 api_key=None
+
+# auth module shortcut
+auth_module = importlib.import_module('.auth', package='tightai')
+auth = auth_module.Auth()
+login = auth.login
+logout = auth.logout
+
+
+# project module shortcut
+projects = importlib.import_module('.projects', package='tightai')
+
+
